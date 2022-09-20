59ca0a020035c8e9cb73a8fadbcd108d92ca1cbd
syosyo10337 <syosyo.ds@gmail.com>
2022-09-21 00:44:36 +0900

    配列をソートする処理を追加しました

--- sort.js
+++ sort.js
@@ -1,1 +1,12 @@
+let sortNumber = function (number) {
+  number.sort(function (a,b) {
+    if (a === b) {
+      return 0;
+    }
+    return a < b ? -1 : 1
+  });
+};
+
 let number = [19,3,56,1,22,09];
+sortNumber(number);
+console.log(number);
